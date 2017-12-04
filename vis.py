d90ba7e0027fc6ccab33cbeff115c09f2cef8204
Eric <twjeric@users.noreply.github.com>
2017-12-03 22:30:11 -0800

    groupby date and sum prediction - just for one id

--- vis.py
+++ vis.py
@@ -4,9 +4,11 @@ import plotly.offline as py
 import plotly.graph_objs as go
 
 # df = pd.read_csv(infile, parse_dates={'datetime': ['date', 'time']}, date_parser=dateparse)
-df = pd.read_csv('result.csv', parse_dates=['dateTime'], index_col='dateTime') 
-df = df.sort_index()
-print(df.head())
+df = pd.read_csv('result.csv', parse_dates=['dateTime'])
+getDate = lambda x: x.date()
+df['dateTime'] = df['dateTime'].map(getDate)
+df = df.groupby('dateTime').sum()
+# print(df.head())
 
 traces_line = []
 
@@ -34,7 +36,9 @@ traces_heat = [
 		x = df.index,
 		y = df.columns,
 		# colorscale = 'Viridis'
-		colorscale=[[0.0, 'rgb(146,168,209)'], [0.1111111111111111, 'rgb(168,185,218)'], [0.2222222222222222, 'rgb(190,203,227)'], [0.3333333333333333, 'rgb(211,220,237)'], [0.4444444444444444, 'rgb(233,238,246)'], [0.5555555555555556, 'rgb(253,244,244)'], [0.6666666666666666, 'rgb(252,234,233)'], [0.7777777777777778, 'rgb(250,223,223)'], [0.8888888888888888, 'rgb(249,213,212)'], [1.0, 'rgb(247,202,201)']]
+		# colorscale=[[0.0, 'rgb(146,168,209)'], [0.1111111111111111, 'rgb(168,185,218)'], [0.2222222222222222, 'rgb(190,203,227)'], [0.3333333333333333, 'rgb(211,220,237)'], [0.4444444444444444, 'rgb(233,238,246)'], [0.5555555555555556, 'rgb(253,244,244)'], [0.6666666666666666, 'rgb(252,234,233)'], [0.7777777777777778, 'rgb(250,223,223)'], [0.8888888888888888, 'rgb(249,213,212)'], [1.0, 'rgb(247,202,201)']]
+		# colorscale=[[0.0, 'rgb(146,168,209)'], [0.25, 'rgb(201,212,232)'], [0.75, 'rgb(251,229,228)'], [1.0, 'rgb(247,202,201)']]
+		colorscale=[[0.0, 'rgb(201,212,232)'], [0.6666666666666666, 'rgb(251,229,228)'], [1.0, 'rgb(247,202,201)']]
 	)
 ]
 

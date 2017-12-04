37ddb70dc5f020a25457e22d4ceb42dd09a78a9f
Eric <twjeric@users.noreply.github.com>
2017-12-04 00:01:50 -0800

    read multiple csv files && log(happiness_index)

--- vis.py
+++ vis.py
@@ -1,14 +1,26 @@
+from math import log
 import numpy as np
 import pandas as pd
 import plotly.offline as py
 import plotly.graph_objs as go
 
 # df = pd.read_csv(infile, parse_dates={'datetime': ['date', 'time']}, date_parser=dateparse)
-df = pd.read_csv('result.csv', parse_dates=['dateTime'])
+df1 = pd.read_csv('./predictions/test_disney_result.csv', parse_dates=['dateTime'])
 getDate = lambda x: x.date()
-df['dateTime'] = df['dateTime'].map(getDate)
-df = df.groupby('dateTime').sum()
+df1['dateTime'] = df1['dateTime'].map(getDate)
+df1 = df1.groupby('dateTime').sum()
+f = lambda x: log(x+1) if x>0 else -log(-x+1)
+df1['prediction'] = df1['prediction'].map(f)
 # print(df.head())
+df2 = pd.read_csv('./predictions/test_trump_result.csv', parse_dates=['dateTime'])
+getDate = lambda x: x.date()
+df2['dateTime'] = df2['dateTime'].map(getDate)
+df2 = df2.groupby('dateTime').sum()
+f = lambda x: log(x+1) if x>0 else -log(-x+1)
+df2['prediction'] = df2['prediction'].map(f)
+
+df = df1.join(df2, how='outer', lsuffix='_l', rsuffix='_r', sort=True)
+df = df.rename(columns = {'prediction_l':'Disney', 'prediction_r':'Trump'})
 
 traces_line = []
 
@@ -37,8 +49,8 @@ traces_heat = [
 		y = df.columns,
 		# colorscale = 'Viridis'
 		# colorscale=[[0.0, 'rgb(146,168,209)'], [0.1111111111111111, 'rgb(168,185,218)'], [0.2222222222222222, 'rgb(190,203,227)'], [0.3333333333333333, 'rgb(211,220,237)'], [0.4444444444444444, 'rgb(233,238,246)'], [0.5555555555555556, 'rgb(253,244,244)'], [0.6666666666666666, 'rgb(252,234,233)'], [0.7777777777777778, 'rgb(250,223,223)'], [0.8888888888888888, 'rgb(249,213,212)'], [1.0, 'rgb(247,202,201)']]
-		# colorscale=[[0.0, 'rgb(146,168,209)'], [0.25, 'rgb(201,212,232)'], [0.75, 'rgb(251,229,228)'], [1.0, 'rgb(247,202,201)']]
-		colorscale=[[0.0, 'rgb(201,212,232)'], [0.6666666666666666, 'rgb(251,229,228)'], [1.0, 'rgb(247,202,201)']]
+		colorscale=[[0.0, 'rgb(146,168,209)'], [0.25, 'rgb(201,212,232)'], [0.75, 'rgb(251,229,228)'], [1.0, 'rgb(247,202,201)']]
+		# colorscale=[[0.0, 'rgb(201,212,232)'], [0.6666666666666666, 'rgb(251,229,228)'], [1.0, 'rgb(247,202,201)']]
 	)
 ]
 
